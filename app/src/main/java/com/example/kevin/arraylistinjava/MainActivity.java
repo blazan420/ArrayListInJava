dc415898ce5f133e5067c9d21dafb12e5a6987bd
kevin <kevinjobrien420@gmail.com>
2018-06-20 17:59:12 -0500

    221 App Number 26 - Part1 - ArrayListInJava // Coding

--- app/src/main/java/com/example/kevin/arraylistinjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/arraylistinjava/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kevin.arraylistinjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView txtInitialCapacity = (TextView) findViewById(R.id.txtInitialCapacity);
+        TextView txtArrayListValues = (TextView) findViewById(R.id.txtArrayListValues);
+        TextView txtArrayListCapacityAfterAddingValues = (TextView) findViewById(R.id.txtArrayListCapacityAfterAddingValues);
+
+
+        // Use this kind of array if I dont know the number of array elements
+        ArrayList<String> animals = new ArrayList<String>();
+
+        txtInitialCapacity.setText(animals.size() + "");
+
+        animals.add("Lion");
+        animals.add("Tiger");
+        animals.add("Cat");
+        animals.add("Panther");
+        animals.add("Bear");
+        animals.add("Bird");
+        animals.add("Fox");
+
+        String oldTxtArrayListValues;
+        for (int index = 0; index < animals.size(); index++) {
+
+            oldTxtArrayListValues = txtArrayListValues.getText().toString();
+            txtArrayListValues.setText(oldTxtArrayListValues + animals.get(index) + "     ");
+
+        }
+
+
+
+
+
+
     }
 }
